8afb43c2a05ee904729ff2bc36b6e2fab92e0958
Dariusz Stefan LEGIZYNSKI <d.legizynski@gmail.com>
2021-03-02 20:19:23 +0100

    added a todo list component

---
+++ src/components/Todos/Todos.vue
@@ -0,0 +1,21 @@
+<template>
+	<div>
+		<div v-for="todo in todos" :key="todo.id">
+			<TodoItem />
+		</div>
+	</div>
+</template>
+
+<script>
+import TodoItem from "../TodoItem/TodoItem";
+
+export default {
+	name: "Todos",
+	components: {
+		TodoItem,
+	},
+	props: ["todos"],
+};
+</script>
+
+<style scoped></style>
