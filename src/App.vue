b065200338e949fe4defafb042188495e7b761b9
Dariusz Stefan LEGIZYNSKI <d.legizynski@gmail.com>
2021-03-03 10:25:37 +0100

    added vue router

--- src/App.vue
+++ src/App.vue
@@ -1,92 +1,40 @@
 <template>
 	<div id="app">
 		<Header />
-		<AddTodo v-on:add-todo="addTodo" />
-		<Todos :todos="todos" v-on:del-todo="deleteTodo" />
+		<router-view />
 	</div>
 </template>
 
 <script>
 import Header from "./components/layout/Header";
-import Todos from "./components/Todos/Todos";
-import AddTodo from "./components/addTodo/AddTodo";
 
 export default {
-	name: "App",
+	name: "app",
 	components: {
-		Todos,
 		Header,
-		AddTodo,
-	},
-	data() {
-		return {
-			todos: [],
-		};
-	},
-	methods: {
-		async deleteTodo(id) {
-			await fetch(
-				`https://jsonplaceholder.typicode.com/todos/${id}`,
-				{
-					method: "DELETE",
-				}
-			).catch((err) => alert(err.message));
-			this.todos = this.todos.filter(
-				(todo) => todo.id !== id
-			);
-		},
-		async addTodo(newTodo) {
-			const { title, completed } = newTodo;
-			const response = await fetch(
-				"https://jsonplaceholder.typicode.com/todos",
-				{
-					method: "POST",
-					headers: {
-						Accept: "application/json",
-						"Content-Type": "application/json",
-					},
-					body: JSON.stringify({ title, completed }),
-				}
-			).catch((err) => alert(err.message));
-			const content = await response.json();
-			this.todos = [...this.todos, content];
-		},
-	},
-	async mounted() {
-		const response = await fetch(
-			"https://jsonplaceholder.typicode.com/todos?_limit=5"
-		).catch((err) => alert(err.message));
-		const fetchedData = await response
-			.json()
-			.catch((err) => alert(err.message));
-		this.todos = fetchedData;
 	},
 };
 </script>
 
 <style>
-* {
-	box-sizing: border-box;
-	margin: 0;
-	padding: 0;
-	scroll-behavior: smooth;
+#app {
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
 }
-body {
-	font-family: Arial, Helvetica, sans-serif;
-	line-height: 1.4;
+
+#nav {
+	padding: 30px;
 }
-.btn {
-	display: inline-block;
-	border: none;
-	background: #555;
-	color: #fff;
-	padding: 7px 20px;
-	cursor: pointer;
+
+#nav a {
+	font-weight: bold;
+	color: #2c3e50;
 }
-.btn:hover {
-	background: #666;
-}
-img {
-	display: block;
+
+#nav a.router-link-exact-active {
+	color: #42b983;
 }
 </style>
---
+++ src/views/Home.vue
@@ -0,0 +1,89 @@
+<template>
+	<div id="app">
+		<AddTodo v-on:add-todo="addTodo" />
+		<Todos :todos="todos" v-on:del-todo="deleteTodo" />
+	</div>
+</template>
+
+<script>
+import Todos from "../components/Todos/Todos";
+import AddTodo from "../components/addTodo/AddTodo";
+
+export default {
+	name: "Home",
+	components: {
+		Todos,
+		AddTodo,
+	},
+	data() {
+		return {
+			todos: [],
+		};
+	},
+	methods: {
+		async deleteTodo(id) {
+			await fetch(
+				`https://jsonplaceholder.typicode.com/todos/${id}`,
+				{
+					method: "DELETE",
+				}
+			).catch((err) => alert(err.message));
+			this.todos = this.todos.filter(
+				(todo) => todo.id !== id
+			);
+		},
+		async addTodo(newTodo) {
+			const { title, completed } = newTodo;
+			const response = await fetch(
+				"https://jsonplaceholder.typicode.com/todos",
+				{
+					method: "POST",
+					headers: {
+						Accept: "application/json",
+						"Content-Type": "application/json",
+					},
+					body: JSON.stringify({ title, completed }),
+				}
+			).catch((err) => alert(err.message));
+			const content = await response.json();
+			this.todos = [...this.todos, content];
+		},
+	},
+	async mounted() {
+		const response = await fetch(
+			"https://jsonplaceholder.typicode.com/todos?_limit=5"
+		).catch((err) => alert(err.message));
+		const fetchedData = await response
+			.json()
+			.catch((err) => alert(err.message));
+		this.todos = fetchedData;
+	},
+};
+</script>
+
+<style>
+* {
+	box-sizing: border-box;
+	margin: 0;
+	padding: 0;
+	scroll-behavior: smooth;
+}
+body {
+	font-family: Arial, Helvetica, sans-serif;
+	line-height: 1.4;
+}
+.btn {
+	display: inline-block;
+	border: none;
+	background: #555;
+	color: #fff;
+	padding: 7px 20px;
+	cursor: pointer;
+}
+.btn:hover {
+	background: #666;
+}
+img {
+	display: block;
+}
+</style>
